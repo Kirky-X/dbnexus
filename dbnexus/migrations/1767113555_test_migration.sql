bdcd8cf2f65fe0e54c985238c19ad8a319ad95a8
Kirky-X <kirky-x@outlook.com>
2025-12-31 01:24:45 +0800

    test: Add test migration files for testing framework
    - Add 12 test migration SQL files for development and testing purposes
    - These migrations are used for integration testing of the migration system
    - Timestamps range from 1767085458 to 1767114584

---
+++ dbnexus/migrations/1767113555_test_migration.sql
@@ -0,0 +1,9 @@
+-- Migration: test_migration
+-- Version: 1767113555
+-- Created: 2025-12-30 16:52:35
+
+-- UP: Apply migration
+-- Your migration SQL goes here
+
+-- DOWN: Rollback migration
+-- Reversal of migration SQL goes here
