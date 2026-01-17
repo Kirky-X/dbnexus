0fd5b13158c4e499f295a756c509893d811f536b
Kirky-X <kirky-x@outlook.com>
2026-01-17 15:40:15 +0800

    chore: update examples to use module paths for sql_parser types
    - Change from pub use imports to module-level access
    - Update all examples to use dbnexus::sql_parser:: path

--- examples/permissions.rs
+++ examples/permissions.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2025 Kirky.X
+//
+// Licensed under the MIT License
+// See LICENSE file in the project root for full license information.
+
 //! 权限控制示例
 //!
 //! 展示如何使用 dbnexus 的权限系统：
--- examples/quickstart.rs
+++ examples/quickstart.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2025 Kirky.X
+//
+// Licensed under the MIT License
+// See LICENSE file in the project root for full license information.
+
 //! 快速开始示例
 //!
 //! 展示 dbnexus 的基本使用方法，包括：
