5f3c124949b32c8a93e5d14432ba4a73d88c5411
Kirky-X <kirky-x@outlook.com>
2026-07-22 11:04:18 +0800

    release: v0.4.3

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@
 
 #![deny(missing_docs)]
 #![forbid(unsafe_code)]
-#![doc(html_root_url = "https://docs.rs/dbnexus/0.4.2")]
+#![doc(html_root_url = "https://docs.rs/dbnexus/0.4.3")]
 
 // ============================================================================
 // 编译期数据库特性互斥检查
